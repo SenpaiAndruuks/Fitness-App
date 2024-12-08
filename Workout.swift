546606a21e41fa77167c0b646d82e4b4fdb91496
Senpai <88218942+SenpaiAndruuks@users.noreply.github.com>
2024-12-07 20:40:02 -0800

    Create Workout.swift

---
+++ Workout.swift
@@ -0,0 +1,15 @@
+//
+//  Workout.swift
+//  Beyond Fit
+//
+//  Created by Andrew on 30/09/2021.
+//
+
+import Foundation
+
+struct Workout {
+    var day: String
+    var bodyPart: String
+    var image: String
+    var routine: [String]
+}
